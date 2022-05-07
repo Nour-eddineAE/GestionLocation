647cf2e1c6ef1169870756c7de2695772bbeae58
Abdelwahed AB <ababdelwahed112@gmail.com>
2022-05-07 17:30:33 +0000

    Add database layout

---
+++ database layout/location.sql
@@ -0,0 +1,163 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 07, 2022 at 05:22 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `location`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `codeClient` int(11) NOT NULL,
+  `nomClient` varchar(30) NOT NULL,
+  `prenomClient` varchar(20) NOT NULL,
+  `adresseClient` varchar(50) NOT NULL,
+  `telClient` varchar(20) NOT NULL,
+  `imageClient` varchar(50) NOT NULL COMMENT 'Path/link to client image'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contrat`
+--
+
+CREATE TABLE `contrat` (
+  `codeContrat` int(11) NOT NULL,
+  `dateContrat` date NOT NULL,
+  `dateEcheance` int(11) NOT NULL,
+  `codeReservation` int(11) NOT NULL,
+  `codeMaticule` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facture`
+--
+
+CREATE TABLE `facture` (
+  `codeFacture` int(11) NOT NULL,
+  `dateFacture` date NOT NULL,
+  `montantFacture` int(11) NOT NULL,
+  `codeContrat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `codeReservation` int(11) NOT NULL,
+  `dateReservation` date NOT NULL,
+  `dateDepReservation` date NOT NULL,
+  `dateRetReservation` date NOT NULL,
+  `isValid` tinyint(1) NOT NULL,
+  `isCanceled` tinyint(1) NOT NULL,
+  `codeClient` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehicule`
+--
+
+CREATE TABLE `vehicule` (
+  `codeMatricule` varchar(20) NOT NULL,
+  `marqueVehicule` varchar(30) NOT NULL,
+  `typeVehicule` varchar(20) NOT NULL,
+  `carburant` varchar(20) NOT NULL,
+  `kilometrage` int(11) NOT NULL,
+  `dateMiseCirculation` date NOT NULL,
+  `codePark` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`codeClient`);
+
+--
+-- Indexes for table `contrat`
+--
+ALTER TABLE `contrat`
+  ADD PRIMARY KEY (`codeContrat`);
+
+--
+-- Indexes for table `facture`
+--
+ALTER TABLE `facture`
+  ADD PRIMARY KEY (`codeFacture`);
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`codeReservation`);
+
+--
+-- Indexes for table `vehicule`
+--
+ALTER TABLE `vehicule`
+  ADD PRIMARY KEY (`codeMatricule`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `client`
+--
+ALTER TABLE `client`
+  MODIFY `codeClient` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `contrat`
+--
+ALTER TABLE `contrat`
+  MODIFY `codeContrat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `facture`
+--
+ALTER TABLE `facture`
+  MODIFY `codeFacture` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `codeReservation` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
