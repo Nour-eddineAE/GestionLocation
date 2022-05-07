df060f322e94617bba0b0f5c2c90772afae571bd
moha-kun <98587728+moha-kun@users.noreply.github.com>
2022-05-07 13:15:52 +0000

    Create ConnectionManager.java

---
+++ src/connectionManager/ConnectionManager.java
@@ -0,0 +1,40 @@
+package connectionManager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionManager {
+	private static String USER = "root";
+	private static String PASS = "root";
+	private static String URL = "jdbc:mysql://localhost:8889/ProjectDB";
+	private static Connection connection;
+
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(URL, USER, PASS);
+		} catch (Exception e) {
+			e.getLocalizedMessage();
+		}
+	}
+
+	public static Connection getConnection() {
+		return connection;
+	}
+
+	public static ResultSet execute(String string) {
+		try {
+			Statement statement = ConnectionManager.getConnection().createStatement();
+			ResultSet result = statement.executeQuery(string);
+			return result;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+}
